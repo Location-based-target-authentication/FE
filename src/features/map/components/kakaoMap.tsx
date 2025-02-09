b6db3699790cd0f27c54e19dc370d95c285ba959
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2025-02-09 19:34:06 +0900

    위치 리스트 및 목표 위치 설정 기능 구현

--- src/features/map/components/kakaoMap.tsx
+++ src/features/map/components/kakaoMap.tsx
@@ -1,11 +1,22 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from "@/components/ui/popover";
+
 import { useEffect, useMemo, useState } from "react";
 
 import Gps from "@/asset/map/gps.svg";
 import positionIconUrl from "@/asset/map/position.svg?url";
 import { debounce } from "es-toolkit";
+import { map } from "es-toolkit/compat";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
 
 function KakaoMap() {
+  const navigate = useNavigate();
+
   const [center, setCenter] = useState({
     lat: 33.450701,
     lng: 126.570667
@@ -16,7 +27,58 @@ function KakaoMap() {
     lng: 126.570667
   });
 
+  const [searchInputValue, setSearchInputValue] = useState("");
+  const [keyword, setKeyword] = useState("");
+  const [positionInfo, setPositionInfo] = useState([
+    {
+      place_name: "",
+      address_name: "",
+      lng: 0,
+      lat: 0
+    }
+  ]);
+
+  const handleReturnToFormPage = async (location) => {
+    if (!location) {
+      toast.error("위치 선택을 다시해주세요.");
+      return;
+    }
+
+    console.log(location);
+    navigate("/", {
+      state: {
+        location
+      }
+    });
+  };
+
   const setCenterToMyPosition = () => setCenter(position);
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") setKeyword(searchInputValue);
+  };
+
+  const getDistance = (lat1, lng1, lat2, lng2) => {
+    function deg2rad(deg) {
+      return deg * (Math.PI / 180);
+    }
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1); // deg2rad below
+    var dLon = deg2rad(lng2 - lng1);
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) *
+        Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+  };
+
+  function formatDistance(km) {
+    return km >= 10 ? `${Math.round(km)}km` : `${km.toFixed(1)}km`;
+  }
 
   const updateCenterWhenMapMoved = useMemo(
     () =>
@@ -38,6 +100,25 @@ function KakaoMap() {
       setPosition({ lat: pos.coords.latitude, lng: pos.coords.longitude });
     });
   }, []);
+
+  useEffect(() => {
+    const places = new kakao.maps.services.Places();
+
+    places.keywordSearch(keyword, (positionInfo, status) => {
+      if (status !== kakao.maps.services.Status.OK) return;
+
+      const positionInfosData = map(
+        positionInfo,
+        ({ place_name, address_name, x, y }) => ({
+          place_name,
+          address_name,
+          lng: Number(x),
+          lat: Number(y)
+        })
+      );
+      setPositionInfo(positionInfosData);
+    });
+  }, [keyword]);
 
   return (
     <div className="flex size-full justify-center">
@@ -72,6 +153,50 @@ function KakaoMap() {
         >
           <Gps />
         </button>
+      </div>
+      <input
+        onChange={(e) => setSearchInputValue(e.target.value)}
+        onKeyDown={(e) => handleKeyPress(e)}
+        value={searchInputValue}
+        placeholder={"주소를 입력해주세요 ex)강남역 or 서울특별시 역삼동"}
+      />
+      <button onClick={() => setKeyword(searchInputValue)}>검색</button>
+      <div className="flex flex-col gap-2">
+        {map(positionInfo, ({ place_name, address_name, lng, lat }) => (
+          <div
+            key={`${place_name} ${address_name}`}
+            className="rounded-lg bg-gray-100 p-4 shadow-md"
+          >
+            <div className="flex justify-between">
+              <div>
+                <div>{place_name}</div>
+                <div>{address_name}</div>
+                <div>
+                  {formatDistance(
+                    getDistance(center.lat, center.lng, lat, lng)
+                  )}
+                </div>
+              </div>
+              <Popover>
+                <PopoverTrigger>Open</PopoverTrigger>
+                <PopoverContent>
+                  <div className="grid gap-4">
+                    <div className="space-y-2">
+                      <h4 className="font-medium leading-none">
+                        해당 위치로 설정하시겠습니까?
+                      </h4>
+                      <button
+                        onClick={() => handleReturnToFormPage({ lat, lng })}
+                      >
+                        확인
+                      </button>
+                    </div>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
