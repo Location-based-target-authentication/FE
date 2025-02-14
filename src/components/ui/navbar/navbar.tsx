d45269c2e32c626393df64bf35d30fef0a8ef5de
Park-min-hyoung <qkralswgud1324@gmail.com>
2025-02-14 20:43:25 +0900

    feat: custom class 생성

--- src/components/ui/navbar/navbar.tsx
+++ src/components/ui/navbar/navbar.tsx
@@ -5,7 +5,7 @@ import { generateNavbarInfo } from "./index.const";
 
 function NavItem({ Icon, label, active, to }) {
   return (
-    <Link to={to} className="flex flex-col items-center text-gray-400">
+    <Link to={to} className="flex flex-1 flex-col items-center text-gray-400">
       <Icon />
       <span
         className={`${active ? "font-semibold text-black" : "text-gray-300"}`}
@@ -21,7 +21,7 @@ function Navbar() {
   const navbarInfo = generateNavbarInfo(location.pathname);
 
   return (
-    <nav className="fixed bottom-0 left-0 flex w-full justify-around border-t bg-white py-3 shadow-md">
+    <nav className="fixed bottom-0 left-1/2 flex w-1/2 -translate-x-1/2 justify-around border-t bg-white py-3 shadow-top">
       {map(navbarInfo, ({ label, activeIcon, notActiveIcon, to, isActvie }) => (
         <NavItem
           key={to}
