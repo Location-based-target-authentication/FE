09039c7cebdadd1ddc985da7dba7d84f3882029e
Park-min-hyoung <qkralswgud1324@gmail.com>
2025-02-16 11:41:43 +0900

    feat: svg 파일 추가 및 버그 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath } from "url";
 
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 const server = {
   port: 8080,
@@ -11,7 +12,7 @@ const server = {
 // https://vitejs.dev/config/
 export default defineConfig({
   server,
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
