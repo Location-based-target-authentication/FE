323af178baca0c5767503302efcaf04ed98918b9
Park-min-hyoung <qkralswgud1324@gmail.com>
2025-02-23 15:43:17 +0900

    feat: 컴포넌트 분리

---
+++ src/components/ui/header/index.const.ts
@@ -0,0 +1,29 @@
+import CoffeIcon from "@/asset/common/coffee.svg?react";
+import Coupon from "@/asset/common/coupon.svg?react";
+import homeBackGroundFirst from "@/asset/home/home-background-first.png?url";
+import homeBackGroundSecond from "@/asset/home/home-background-second.png?url";
+import homeBackGroundThird from "@/asset/home/home-background-third.png?url";
+
+export const generateKeyword = (point) => {
+  if (point <= 500) return "coffee";
+  else if (point > 500) return "convenienceStore";
+  else return "reward";
+};
+
+export const generateIcon = (keyword) => {
+  if (keyword === "coffee") return CoffeIcon;
+  else if (keyword === "convenienceStore") return Coupon;
+  else return Coupon;
+};
+
+export const generateProgressPercent = (point) => {
+  if (point <= 500) return (point / 500) * 100;
+  else if (point > 500) return (point / 1000) * 100;
+  else return 100;
+};
+
+export const backgroundImages = [
+  homeBackGroundFirst,
+  homeBackGroundSecond,
+  homeBackGroundThird
+];
--- src/features/home/components/index.const.ts
+++ src/features/home/components/index.const.ts
@@ -1,3 +1,5 @@
+import { join, map, replace } from "es-toolkit/compat";
+
 export const DAYS_STRING_MAP = new Map([
   ["MON", "월"],
   ["TUE", "화"],
@@ -7,3 +9,17 @@ export const DAYS_STRING_MAP = new Map([
   ["SAT", "토"],
   ["SUN", "일"]
 ]);
+
+export const generatDdateText = (startDate, endDate) => {
+  const replacedStartDate = replace(startDate, /-/g, ". ");
+  const replacedEndDate = replace(endDate, /-/g, ". ");
+
+  return `${replacedStartDate} ~ ${replacedEndDate}`;
+};
+
+export const generateDayText = (days) => {
+  const transformDays = map(days, (day) => DAYS_STRING_MAP.get(day));
+  const day = days.length === 7 ? "매일" : join(transformDays, ", ");
+
+  return day;
+};
