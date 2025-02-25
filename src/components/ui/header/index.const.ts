0c557d01f55a14438dd77138a66193bc8c9b9c58
Park-min-hyoung <qkralswgud1324@gmail.com>
2025-02-25 21:30:17 +0900

    feat: header 설정

--- src/components/ui/header/index.const.ts
+++ src/components/ui/header/index.const.ts
@@ -4,5 +4,6 @@ export const NOT_VISIBLE_HEADER_PAGES = [paths.map.search.getHref()];
 
 export const HEADER_TITLE_MAP = new Map([
   [paths.home.getHref(), "정보입력"],
-  [paths.map.certification.getHref(), "목표인증"]
+  [paths.map.certification.getHref(), "목표인증"],
+  [paths.profile.reward.getHref(), "리워드 신청"]
 ]);
