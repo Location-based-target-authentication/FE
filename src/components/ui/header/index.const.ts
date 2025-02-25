31b835976c2eaabed4d4210ce2ec80c1b75bcbef
Park-min-hyoung <qkralswgud1324@gmail.com>
2025-02-25 10:48:35 +0900

    feat: 목표 인증 페이지 header 설정

--- src/components/ui/header/index.const.ts
+++ src/components/ui/header/index.const.ts
@@ -2,4 +2,7 @@ import { paths } from "@/config/paths";
 
 export const NOT_VISIBLE_HEADER_PAGES = [paths.map.search.getHref()];
 
-export const HEADER_TITLE_MAP = new Map([[paths.home.getHref(), "정보입력"]]);
+export const HEADER_TITLE_MAP = new Map([
+  [paths.home.getHref(), "정보입력"],
+  [paths.map.certification.getHref(), "목표인증"]
+]);
