ee0a61b7a127b8902a26e8cc73d61719e5c22ebc
박민형 <qkralswgud1324@gmail.com>
2025-02-25 10:34:22 +0900

    [feat/header-layout] header layout 설정 (#8)
    * feat: editorconfig 수정
    
    * faet: husky 수정
    
    * feat: haeader layout 제작
    
    * feat: pre-push 테스팅
    
    * feat: 코드 복구

---
+++ src/components/ui/header/index.const.ts
@@ -0,0 +1,5 @@
+import { paths } from "@/config/paths";
+
+export const NOT_VISIBLE_HEADER_PAGES = [paths.map.search.getHref()];
+
+export const HEADER_TITLE_MAP = new Map([[paths.home.getHref(), "정보입력"]]);
