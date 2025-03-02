54c94ee94e2b448091252e5769f5a3c0e05511ca
박민형 <qkralswgud1324@gmail.com>
2025-03-02 13:47:26 +0900

    [feat/map-certificaiton-update] 목표 인증 메뉴 보완 (#24)
    * feat: 인증완료시 포인트 및 데이터 업데이트
    
    * feat: 장소 검색 리스트가 최대 10개만 보이도록 수정

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -5,6 +5,7 @@ interface UserState {
   point: number;
   userId: string;
   setPoint: (point: number) => void;
+  addPoint: (addPoint: number) => void;
   setUserName: (userName: string) => void;
 }
 
@@ -13,5 +14,7 @@ export const useUserStore = create<UserState>((set) => ({
   point: 100000,
   userId: "0",
   setPoint: (point) => set({ point }),
+  addPoint: (addPoint: number) =>
+    set((state) => ({ point: state.point + addPoint })),
   setUserName: (userName) => set({ userName })
 }));
